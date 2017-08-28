7eac77aa1b3187c7251c06445c99920116d572a1
Seven Du <shiweidu@outlook.com>
2017-08-28 11:23:45 +0800

    完成基础构件。

---
+++ webpack.config.babel.js
@@ -0,0 +1,102 @@
+/*
+|--------------------------------------------------
+| ThinkSNS+ Documentation.
+|--------------------------------------------------
+|
+| 为了保障整个文档 javaScript 的统一性，所有与 JS 想关
+| 的配置文件都应该使用 ES6 代码进行编写，例如 Webpack
+| 配置文件。
+|
+*/
+
+import webpack from 'webpack';
+import path from 'path';
+
+// 环境变量
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isHot = process.argv.includes('--hot');
+const isProd = NODE_ENV === 'production';
+
+export default {
+
+  // devtool
+  // 参考 https://webpack.js.org/configuration/devtool/#devtool
+  devtool: isProd ? false : 'source-map',
+
+  // 入口配置
+  // 参考 https://webpack.js.org/configuration/entry-context/#entry
+  entry: path.resolve(__dirname, 'src/main.js'),
+
+  // 输出配置
+  // 参考 https://webpack.js.org/configuration/output
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'assets'),
+  },
+
+  // 模块配置
+  // 参考 https://webpack.js.org/configuration/module/
+  module: {
+
+    // 模块加载器设置
+    // 参考 https://webpack.js.org/configuration/module/#module-rules
+    rules: [
+      {
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        include: [ resolve.resolve(__dirname, 'src') ],
+        options: {
+          presets: [ 'es2015' ]
+        }
+      }
+    ]
+  }
+
+  // 插件配置
+  // 参考 https://webpack.js.org/configuration/plugins/#plugins
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(NODE_ENV),
+      },
+    }),
+
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js',
+    }),
+
+    // 根据环境变量选择插件
+    ...(isProd ? [ // 生产环境
+
+      // build optimization plugins
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: false,
+        },
+        sourceMap: false,
+      }),
+
+    ] : [ // 开发环境
+      new webpack.NoEmitOnErrorsPlugin(),
+    ]),
+
+    // webpack-dev-server enhancement plugins
+    ...(! isHot ? [] : [
+      new webpack.HotModuleReplacementPlugin()
+    ]),
+  ],
+
+  // devServer 配置
+  // 参考 https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    contentBase: path.join(__dirname, 'assets'), // // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+  }
+};
