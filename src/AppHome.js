fe414a9ee119f3c8170b4ca000e9b2656f65b499
Seven Du <shiweidu@outlook.com>
2017-08-30 17:31:37 +0800

    友好的首页开发完成。

--- src/AppHome.js
+++ src/AppHome.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 // Material UI
 import withStyles from 'material-ui/styles/withStyles';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 
-const styles = () => ({
+// Icons
+import CloudDownloadIcon from 'material-ui-icons/CloudDownload';
+import StarIcon from 'material-ui-icons/Star';
+
+// axios
+import { get as getRequest } from 'axios';
+
+const styles = theme => ({
   root: {
     flex: '1 0 100%'
   },
@@ -48,13 +56,36 @@ const styles = () => ({
   title: {
     maxWidth: 500,
     textAlign: 'center',
+  },
+  buttonBox: {
+    marginTop: 24
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  starCount: {
+    position: 'relative',
+    textTransform: 'none',
+    '&::before': {
+      content: " ",
+      display: 'block',
+      position: 'absolute',
+      top: 0,
+      left: -5
+    }
   }
 });
 
 class AppHome extends Component {
+
+  state = {
+    starCount: 0
+  };
+
   render() {
 
     const { classes } = this.props;
+    console.log(this.state);
 
     return (
       <div className={classes.root}>
@@ -64,9 +95,46 @@ class AppHome extends Component {
             <Typography className={classes.inheritColor} type="display2">ThinkSNS+</Typography>
             <Typography className={`${classes.inheritColor} ${classes.title}`} type="subheading">基于 Laravel 开发的用户生态系统</Typography>
           </div>
+          <div className={classes.buttonBox}>
+            <Button className={classes.button} raised color="primary" href="https://github.com/slimkit/thinksns-plus/releases">
+              <CloudDownloadIcon /> &nbsp; Download
+            </Button>
+            <Button className={classes.button} raised color="accent" href="https://github.com/slimkit/thinksns-plus/stargazers">
+              <StarIcon /> &nbsp; Star&nbsp;{this.getStarCount()}
+            </Button>
+          </div>
         </div>
       </div>
     );
+  }
+
+  getStarCount() {
+    const { starCount } = this.state;
+
+    if (! starCount) {
+      return '';
+    }
+
+    if (starCount >= 10000000) {
+      return Math.round(starCount / 10000000) + 'KW';
+    } else if (starCount >= 100000) {
+      return Math.round(starCount / 10000) + 'W';
+    } else if (starCount >= 1000) {
+      return Math.round(starCount / 1000) + 'K';
+    }
+
+    return parseInt(starCount);
+
+  }
+
+  componentDidMount() {
+    getRequest(
+      'https://api.github.com/repos/slimkit/thinksns-plus'
+    ).then(({ data: { stargazers_count } }) => {
+
+      this.setState({ starCount: stargazers_count });
+
+    });
   }
 }
 
