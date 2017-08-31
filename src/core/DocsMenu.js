ee0c889ae06ef77d4f100dbf0a140a670db0bd56
Seven Du <shiweidu@outlook.com>
2017-08-31 17:48:40 +0800

    完成核心文档基础UI

--- src/core/DocsMenu.js
+++ src/core/DocsMenu.js
@@ -1,49 +1,6 @@
-import React, { Component, cloneElement } from 'react';
-
-// Material UI
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
-import Button from 'material-ui/Button';
-import Collapse from 'material-ui/transitions/Collapse';
-import ExpandLessIcon from 'material-ui-icons/ExpandLess';
-import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
-
-class DocsMenuItem extends Component {
-
-  state = { expand: false };
-
-  render() {
-
-    const { tree, prefix } = this.props;
-    const { expand = false } = this.state;
-
-    if (!! tree.items) {
-      return (
-        <div>
-          <ListItem style={{ width: '100%' }} component={Button} onClick={() => this.handleExpandClick()}>
-            <ListItemText style={{ textAlign: 'left' }} primary={tree.name} />
-            <ListItemIcon>
-              { expand ? (<ExpandLessIcon />) : (<ExpandMoreIcon />) }
-            </ListItemIcon>
-          </ListItem>
-          <Collapse transitionDuration="auto" unmountOnExit in={expand}>
-            <DocsMenu tree={tree.items} prefix={`${prefix}/${tree.link}`} style={{ paddingLeft: 16 }} />
-          </Collapse>
-        </div>
-      );
-    }
-
-    return (
-      <ListItem button component="div" href={`${prefix}/${tree.link}`}>
-        <ListItemText primary={tree.name} />
-      </ListItem>
-    );
-
-  }
-
-  handleExpandClick() {
-    this.setState({ expand: ! this.state.expand });
-  }
-}
+import React, { Component } from 'react';
+import List from 'material-ui/List';
+import DocsMenuItem from './DocsMenuItem';
 
 class DocsMenu extends Component {
   render() {
@@ -63,29 +20,3 @@ class DocsMenu extends Component {
 }
 
 export default DocsMenu;
-
-
-/*
-<ListItem component="div">
-            <ListItemText primary="开始使用" />
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-          </ListItem>
-          <List style={{ padding: "0 16px" }}>
-            <ListItem button>
-              <ListItemText primary="关于 ThinkSNS+" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="安装" />
-            </ListItem>
-          </List>
-
-
-        <ListItem component="div" button>
-          <ListItemText primary="开始使用" />
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-        </ListItem>
- */
--- src/core/Main.js
+++ src/core/Main.js
@@ -4,11 +4,31 @@ import DocsMenu from './DocsMenu';
 
 const styles = theme => ({
   root: {
-    width: '100%'
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row'
   },
   menu: {
     width: 256,
-    paddingLeft: theme.spacing.unit * 2
+    paddingTop: theme.spacing.unit * 4,
+    paddingLeft: theme.spacing.unit * 2,
+    display: 'flex',
+    justifyContent: 'flex-end',
+    boxSizing: 'border-box',
+    flexShrink: 0,
+  },
+  divider: {
+    width: 1,
+    height: 'auto',
+    background: '#eaeaea',
+    marginLeft: theme.spacing.unit
+  },
+  docs: {
+    width: '100%',
+    flexShrink: 1,
+    paddingTop: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+    paddingLeft: theme.spacing.unit * 3
   }
 });
 
@@ -53,7 +73,10 @@ class Main extends Component {
         
         <div className={classes.menu}>
           <DocsMenu tree={tree} prefix="" />
+          <span className={classes.divider} />
         </div>
+
+        <div className={classes.docs}>Markdown.</div>
 
       </div>
     );
