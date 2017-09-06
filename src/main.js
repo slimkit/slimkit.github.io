78aaecc10fdf4cfc8bcdc5e2172ef6d40edc381b
Seven Du <shiweidu@outlook.com>
2017-09-06 15:08:53 +0800

    修改前端路由 basename。

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import store from './createStore';
 import App from './App';
 
 render(
-  (<BrowserRouter>
+  (<BrowserRouter basename={document.querySelector('#basename').href}>
     <ReduxProvider store={store}>
       <MuiThemeProvider theme={createMuiTheme()}>
         <App />
